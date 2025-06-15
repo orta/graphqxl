14b19f7245da46f81e5a0cda43d0bb619ed511f3
Orta Therox <git@orta.io>
2025-06-15 17:08:02 +0100

    0.1.3

--- graphqxl-napi/example.js
+++ graphqxl-napi/example.js
@@ -1,9 +1,9 @@
-const { graphqxlToSdl } = require('./index.js');
+const { graphqxlToSdl } = require("./index.js");
 
 // Example 1: Basic schema
-console.log('=== Example 1: Basic Schema ===');
+console.log("=== Example 1: Basic Schema ===");
 const basicVfs = {
-  'schema.graphqxl': `
+  "schema.graphqxl": `
     type User {
       id: ID!
       name: String!
@@ -14,17 +14,17 @@ const basicVfs = {
       user(id: ID!): User
       users: [User!]!
     }
-  `
+  `,
 };
 
-const basicResult = graphqxlToSdl(basicVfs, 'schema.graphqxl');
+const basicResult = graphqxlToSdl(basicVfs, "schema.graphqxl");
 console.log(basicResult);
 console.log();
 
-// Example 2: Schema with imports
-console.log('=== Example 2: Schema with Imports ===');
+// Example 2: Schema with included imports
+console.log("=== Example 2: Schema with Imports ===");
 const importVfs = {
-  'schema.graphqxl': `
+  "schema.graphqxl": `
     import "models/user"
     import "models/post"
     
@@ -33,30 +33,30 @@ const importVfs = {
       post(id: ID!): Post
     }
   `,
-  'models/user.graphqxl': `
+  "models/user.graphqxl": `
     type User {
       id: ID!
       name: String!
       posts: [Post!]!
     }
   `,
-  'models/post.graphqxl': `
+  "models/post.graphqxl": `
     type Post {
       id: ID!
       title: String!
       author: User!
     }
-  `
+  `,
 };
 
-const importResult = graphqxlToSdl(importVfs, 'schema.graphqxl');
+const importResult = graphqxlToSdl(importVfs, "schema.graphqxl");
 console.log(importResult);
 console.log();
 
 // Example 3: Using generics
-console.log('=== Example 3: Using Generics ===');
+console.log("=== Example 3: Using Generics ===");
 const genericVfs = {
-  'schema.graphqxl': `
+  "schema.graphqxl": `
     type Response<T> {
       data: T
       error: String
@@ -72,17 +72,17 @@ const genericVfs = {
     type Query {
       getUser(id: ID!): UserResponse!
     }
-  `
+  `,
 };
 
-const genericResult = graphqxlToSdl(genericVfs, 'schema.graphqxl');
+const genericResult = graphqxlToSdl(genericVfs, "schema.graphqxl");
 console.log(genericResult);
 console.log();
 
 // Example 4: With custom indent and private prefix
-console.log('=== Example 4: Custom Options ===');
+console.log("=== Example 4: Custom Options ===");
 const optionsVfs = {
-  'schema.graphqxl': `
+  "schema.graphqxl": `
     type User {
       id: ID!
       name: String!
@@ -92,8 +92,8 @@ const optionsVfs = {
     type Query {
       user(id: ID!): User
     }
-  `
+  `,
 };
 
-const optionsResult = graphqxlToSdl(optionsVfs, 'schema.graphqxl', 4, '__');
+const optionsResult = graphqxlToSdl(optionsVfs, "schema.graphqxl", 4, "__");
 console.log(optionsResult);
